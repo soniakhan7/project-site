84e7bbedd376894e75a26e4d8a83694424190b67
Sonia Khan <soninony@gmail.com>
2023-04-18 17:32:51 -0400

    temp commit

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post("/", async (req, res) => {
     res.send(completion.data.choices[0].text);
 });
 
-const port = 8080
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 })
