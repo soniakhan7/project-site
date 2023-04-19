6ffe825a87ddd6a7401e2e8089d2e1ceec538a87
Allen824 <31705844+Allen824@users.noreply.github.com>
2023-04-19 14:11:17 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import {useState} from 'react'
 import Home from './LandingPage.js'
+import QuizFront from './Quiz-front.js'
 import Quiz from './Quiz.js'
 import Results from './Last-page-of-quiz.js'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
@@ -10,14 +11,16 @@ function App() {
     const [name, setName] = useState('')
     const [response, setResponse] = useState('Loading your meal plan...')
 
+
     return (
     <div>
         <BrowserRouter>
         <div>
             <Routes>
             <Route path="/" element ={<Home/>} />
-            <Route path="/Quiz" element ={<Quiz setName={setName} setResponse={setResponse}/>} />
-            <Route path="/Results" element={<Results response={response} name={name}/>} />
+            <Route path="/QuizFront" element={<QuizFront/>} />
+            <Route path="/Quiz" element ={<Quiz name={name} setName={setName} setResponse={setResponse}/>} />
+            <Route path="/Results" element={<Results response={response} name={name} setName={setName} setResponse={setResponse}/>} />
             </Routes>
         </div>
     </BrowserRouter>
