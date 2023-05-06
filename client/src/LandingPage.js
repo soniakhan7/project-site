7c1221fdb26813193510a076a42e1aa25c8fb138
Sonia Khan <39599357+soniakhan7@users.noreply.github.com>
2023-05-05 21:09:41 -0400

    Revert "Revert "Feat/adjust css and templates""

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -1,4 +1,7 @@
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import './footer.css'
 import './nav.css'
 import './home-main.css'
@@ -52,7 +55,7 @@ function Home () {
             </head>
             <body>
             {/*Nav section of site*/}
-    <header>
+    <div className="nav">
         {/*left-side nav*/}
         <div class="left-nav-sec">
             <div class="nav-logo">
@@ -63,10 +66,10 @@ function Home () {
         {/*right-side nav*/}
         <div class="right-nav-sec">
             <div>
-                <Link class="quiz-cta-btn" to="/QuizFront"> Take the quiz </Link>
+                <Link className="quiz-cta-btn" to="/QuizFront"> Take the quiz </Link>
             </div>
         </div>
-    </header>
+    </div>
 
     {/*Hero section*/}
     <section class="hero-sec">
@@ -83,7 +86,7 @@ function Home () {
                     and more convenient for you. Take our quiz and create a free 5-day personalized meal plan today!
                 </p>
 
-                <a class="hero-cta-btn" href="form-entry.html"> Take the quiz </a>
+                <div className='hero-button'><Link to="/QuizFront" class="hero-cta-btn"> Take the quiz </Link></div>
             </div>
             <div class="hero-trustmarks">
                 <span>
@@ -321,9 +324,8 @@ function Home () {
 
             <div class="faq-container">
                 <div class="faq-item">
-                    <div class="faq-question" onClick={() => handleArr(0)}>
-                        <button class="faq-toggle"><i class="fa fa-chevron-right"></i>
-                        </button>
+                    <div class="faq-question">
+                        <span className='faq-arrow' onClick={() => handleArr(0)}><FontAwesomeIcon icon={open[0] ? faChevronRight : faChevronDown} /></span>
                         <h3>What makes better health different from others?</h3>
                     </div>
                     <div className={`faq-answer ${open[0] ? 'inactive' : 'active'}`}>
@@ -333,8 +335,8 @@ function Home () {
                     </div>
                 </div>
                 <div class="faq-item">
-                    <div class="faq-question" onClick={() => handleArr(1)}>
-                        <button class="faq-toggle"><i class="fa fa-chevron-right"></i></button>
+                    <div class="faq-question">
+                        <span className='faq-arrow' onClick={() => handleArr(1)}><FontAwesomeIcon icon={open[1] ? faChevronRight : faChevronDown} /></span>
                         <h3>How does the quiz work? </h3>
                     </div>
                     <div className={`faq-answer ${open[1] ? 'inactive' : 'active'}`}>
@@ -345,8 +347,8 @@ function Home () {
                     </div>
                 </div>
                 <div class="faq-item">
-                    <div class="faq-question" onClick={() => handleArr(2)}>
-                        <button class="faq-toggle"><i class="fa fa-chevron-right"></i></button>
+                    <div class="faq-question">
+                        <span className='faq-arrow' onClick={() => handleArr(2)}><FontAwesomeIcon icon={open[2] ? faChevronRight : faChevronDown} /></span>
                         <h3>Is there a limit to the number of meal plans that I can create? </h3>
                     </div>
                     <div className={`faq-answer ${open[2] ? 'inactive' : 'active'}`}>
@@ -355,8 +357,8 @@ function Home () {
                     </div>
                 </div>
                 <div class="faq-item">
-                    <div class="faq-question" onClick={() => handleArr(3)}>
-                        <button class="faq-toggle"><i class="fa fa-chevron-right"></i></button>
+                    <div class="faq-question">
+                        <span className='faq-arrow' onClick={() => handleArr(3)}><FontAwesomeIcon icon={open[3] ? faChevronRight : faChevronDown} /></span>
                         <h3>What diets are supported?</h3>
                     </div>
                     <div className={`faq-answer ${open[3] ? 'inactive' : 'active'}`}>
@@ -373,8 +375,8 @@ function Home () {
                     </div>
                 </div>
                 <div class="faq-item">
-                    <div class="faq-question" onClick={() => handleArr(4)}>
-                        <button class="faq-toggle"><i class="fa fa-chevron-right"></i></button>
+                    <div class="faq-question">
+                        <span className='faq-arrow' onClick={() => handleArr(4)}><FontAwesomeIcon icon={open[4] ? faChevronRight : faChevronDown} /></span>
                         <h3>What kind of recipes are included in the meal plan?</h3>
                     </div>
                     <div class={`faq-answer ${open[4] ? 'inactive' : 'active'}`}>
@@ -383,8 +385,8 @@ function Home () {
                     </div>
                 </div>
                 <div class="faq-item">
-                    <div class="faq-question" onClick={() => handleArr(5)}>
-                        <button class="faq-toggle"><i class="fa fa-chevron-right"></i></button>
+                    <div class="faq-question">
+                        <span className='faq-arrow' onClick={() => handleArr(5)}><FontAwesomeIcon icon={open[5] ? faChevronRight : faChevronDown} /></span>
                         <h3>Is there support available if I have questions or need help with my meal plan?</h3>
                     </div>
                     <div class={`faq-answer ${open[5] ? 'inactive' : 'active'}`}>
